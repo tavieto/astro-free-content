625f51934faf6a9e4f265d1a49fb5d8df8ed20fa
Otávio <otaviogabriel@ioasys.com.br>
2022-07-04 17:13:17 -0300

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ContentsModule } from './contents/contents.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ContentsModule, MongooseModule.forRoot('mongodb+srv://tavieto:15ioasys86@cluster0.wtub9xi.mongodb.net/?retryWrites=true&w=majority')],
+  imports: [ContentsModule, MongooseModule.forRoot('mongodb+srv://goastro:15ioasys86@cluster0.wtub9xi.mongodb.net/?retryWrites=true&w=majority')],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000, () => {
-    console.log("PORT = ", process.env.PORT)
-  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
